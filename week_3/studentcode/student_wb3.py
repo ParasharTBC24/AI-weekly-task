2998a2d9fbf3840b171acbc9d1d02aaa59e00338
ParasharTBC24 <bparashar24@tbc.edu.np>
2025-05-14 20:52:47 +0545

    Week 3

---
+++ week_3/studentcode/student_wb3.py
@@ -0,0 +1,35 @@
+
+from approvedimports import *
+
+class DepthFirstSearch(SingleMemberSearch):
+    """your implementation of depth first search to extend
+    the superclass SingleMemberSearch search.
+    Adds  a __str__method
+    Over-rides the method select_and_move_from_openlist
+    to implement the algorithm
+    """
+
+    def __str__(self):
+        return "depth-first"
+
+    def select_and_move_from_openlist(self) -> CandidateSolution:
+        """void in superclass
+        In sub-classes should implement different algorithms
+        depending on what item it picks from self.open_list
+        and what it then does to the openlist
+
+        Returns
+        -------
+        next working candidate (solution) taken from openlist
+        """
+
+        # create a candidate solution variable to hold the next solution
+        next_soln = CandidateSolution()
+
+        # ====> insert your pseudo-code and code below here
+        my_index = -1 #select last item and remove it from the list
+        next_soln = self.open_list[my_index]
+        self.open_list.pop(my_index)
+
+        # <==== insert your pseudo-code and code above here
+        return next_soln
